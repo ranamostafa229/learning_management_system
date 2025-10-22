ed6ab281f744b4d92e42e59d438860f95bc3f0d4
Your Name <ranamostafa2210@gmail.com>
2025-10-22 23:48:21 +0300

    feat: add request protection with bot detection and rate limiting for S3 upload and delete endpoints

--- app/api/s3/delete/route.ts
+++ app/api/s3/delete/route.ts
@@ -1,10 +1,38 @@
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import { auth } from "@/lib/auth";
 import { env } from "@/lib/env";
 import { S3 } from "@/lib/S3Client";
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { error } from "console";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5, // max 5 requests per window (1 minute)
+    })
+  );
 export async function DELETE(request: Request) {
   try {
+    const session = await auth.api.getSession({ headers: await headers() });
+    const decision = await aj.protect(request, {
+      fingerprint: session?.user?.id!,
+    });
+    if (decision.isDenied()) {
+      return NextResponse.json(
+        { error: "Not allowed to delete" },
+        { status: 429 }
+      );
+    }
     const body = await request.json();
     const { key } = body;
     if (!key) {
--- app/api/s3/upload/route.ts
+++ app/api/s3/upload/route.ts
@@ -4,9 +4,36 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3 } from "@/lib/S3Client";
 import { v4 as uuidv4 } from "uuid";
 import { env } from "@/lib/env";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5, // max 5 requests per window (1 minute)
+    })
+  );
 export async function POST(request: Request) {
+  const session = await auth.api.getSession({ headers: await headers() });
   try {
+    const decision = await aj.protect(request, {
+      fingerprint: session?.user?.id!,
+    });
+    if (decision.isDenied()) {
+      return NextResponse.json(
+        { error: "Not allowed to upload" },
+        { status: 429 }
+      );
+    }
     const body = await request.json();
     const { fileName, contentType, size } = body;
     const key = `${uuidv4()}-${fileName}`;
