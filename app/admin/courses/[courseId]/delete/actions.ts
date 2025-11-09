9f7fc0c2dba1ed4a36a7a753e134c2127bb01975
Your Name <ranamostafa2210@gmail.com>
2025-11-09 21:29:31 +0200

    feat: add request protection with bot detection and rate limting by Arcjet for course deleting

--- app/admin/courses/[courseId]/delete/actions.ts
+++ app/admin/courses/[courseId]/delete/actions.ts
@@ -1,12 +1,48 @@
 "use server";
 import { requireAdmin } from "@/app/data/admin/require-admin";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
 import { prisma } from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
+import { request } from "@arcjet/next";
 import { revalidatePath } from "next/cache";
 
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5, // max 5 requests per window (1 minute)
+    })
+  );
+
 export async function deleteCourse(courseId: string): Promise<ApiResponse> {
-  await requireAdmin();
+  const session = await requireAdmin();
   try {
+    const req = await request();
+    const decision = await aj.protect(req, {
+      fingerprint: session?.user?.id!,
+    });
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return {
+          status: "error",
+          message:
+            "You have blocked due to too many requests. Please try again later.",
+        };
+      } else {
+        return {
+          status: "error",
+          message: "bot-like behavior detected. Action denied",
+        };
+      }
+    }
+
     await prisma.course.delete({
       where: {
         id: courseId,
