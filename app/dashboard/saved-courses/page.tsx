4fe2b2f7f21263cf5d5731ca2ff6ff2632dfa20e
Your Name <ranamostafa2210@gmail.com>
2025-11-26 22:29:26 +0200

    feat(saved-courses): enhance SavedCourses component with EmptyState for better user experience when no courses are saved

--- app/dashboard/saved-courses/page.tsx
+++ app/dashboard/saved-courses/page.tsx
@@ -5,20 +5,31 @@ import {
 } from "@/app/data/user/get-saved-courses";
 import { Separator } from "@/components/ui/separator";
 import RemoveAllCourses from "./_components/RemoveAllCourses";
+import EmptyState from "@/components/general/EmptyState";
+import { cn } from "@/lib/utils";
 
 const SavedCourses = async () => {
   const courses = await getSavedCourses();
 
   return (
-    <section className="flex flex-wrap lg:flex-nowrap gap-4">
-      <div className="w-full lg:w-96 flex flex-col gap-4 ">
+    <section
+      className={cn(
+        "flex flex-wrap lg:flex-nowrap gap-4",
+        courses.length === 0 && "flex flex-col"
+      )}
+    >
+      <div
+        className={cn(
+          "w-full lg:w-96 flex flex-col gap-4 ",
+          courses.length === 0 && "lg:w-full"
+        )}
+      >
         <div className="flex justify-between">
           <h4 className="font-medium">Saved Courses</h4>
           <span>{courses.length} Courses</span>
         </div>
         <Separator className="bg-border dark:bg-white" />
-        {/* <Button> Remove All Courses</Button> */}
-        <RemoveAllCourses />
+        {courses.length > 0 && <RemoveAllCourses />}
       </div>
       <RenderSavedCourses courses={courses} />
     </section>
@@ -28,6 +39,17 @@ const SavedCourses = async () => {
 export default SavedCourses;
 
 const RenderSavedCourses = ({ courses }: { courses: SavedCoursesType }) => {
+  if (courses.length === 0) {
+    return (
+      <div className="w-full">
+        <EmptyState
+          description="No saved courses found. Save a course to get started!"
+          buttonText={"Explore Courses"}
+          href={"/courses"}
+        />
+      </div>
+    );
+  }
   return (
     <div className="w-full space-y-4">
       {courses.map(({ course }) => (
