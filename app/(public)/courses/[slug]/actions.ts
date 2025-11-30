4f0f7f05f620609148cc45690127bf487e45af51
Your Name <ranamostafa2210@gmail.com>
2025-12-01 00:53:36 +0200

    fix(actions): correct Stripe error handling and update import statement feat(enrollment): add initial route for verifying enrollment

--- app/(public)/courses/[slug]/actions.ts
+++ app/(public)/courses/[slug]/actions.ts
@@ -7,7 +7,6 @@ import { stripe } from "@/lib/stripe";
 import { ApiResponse } from "@/lib/types";
 import { request } from "@arcjet/next";
 import { redirect } from "next/navigation";
-import Stripe from "stripe";
 
 const aj = arcjet.withRule(
   fixedWindow({
@@ -148,7 +147,7 @@ export async function enrollInCourseAction(
 
     checkoutUrl = result.checkoutUrl as string;
   } catch (error) {
-    if (error instanceof Stripe.errors.StripeError) {
+    if (error instanceof stripe.errors.StripeError) {
       return {
         status: "error",
         message: "Payment system error, please try again later.",
